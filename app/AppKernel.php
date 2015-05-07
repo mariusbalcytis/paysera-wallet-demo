1037528df67016f4004ebd2509207ddb6a7afd96
m.balcytis <marius.balcytis@gmail.com>
2015-05-07 03:36:27 +0300

    Integrated Paysera Wallet bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Maba\Bundle\PayseraWalletDemoBundle\MabaPayseraWalletDemoBundle(),
+            new Paysera\Bundle\WalletBundle\PayseraWalletBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
